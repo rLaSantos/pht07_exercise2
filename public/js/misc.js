56c9cf897a2a503f0f375b2d8de36914d9439f0d
rLaSantos <marlalmaydasantos@gmail.com>
2024-06-14 14:33:22 +0800

    :recycle: Refactor Codes

--- public/js/misc.js
+++ public/js/misc.js
@@ -1,43 +1,74 @@
-    let baseUrl = window.location.origin;
-    let pathName = window.location.pathname;
-    let page = pathName.split("/").pop();
+// //Paths
+let baseUrl = window.location.origin;
+let home = baseUrl + "/views/home.html";
+let pathName = window.location.pathname;
+let page = pathName.split("/").pop();
 
-    if (page == "dashboard.html") {
+// //Variables
+let nickName = localStorage.getItem("name");
 
-        let name = localStorage.getItem("name");
+// //Validate if local Sotrage have name and in page is in home
+if (page == "home.html") {
 
-        if (name) {
+    if (nickName) {
 
-            document.getElementById("target").innerText = name.toUpperCase() + "!";
-        }
+        document.getElementById("target").innerText = nickName.toUpperCase() + "!";
+    } else {
+
+        location.href = baseUrl;
+        alert("Nickname is required!");
+    }
+} else {
+
+    if (nickName) {
+
+        location.href = home
+    }
+}
+
+// //Enter Function in Index
+function enter() {
+
+    let name = document.getElementById("name").value;
+
+    if (name) {
+
+        localStorage.setItem("name", name);
+        location.href = home;
+    } else {
+
+        alert("Nickname is required!");
+    }
+}
+
+// //Return to Index and remove localstorage item/s
+function index() {
+
+    localStorage.removeItem("name");
+    location.href = baseUrl
+}
+
+// //Will Show/Hide and add/remove "tabs-open" class div connected to button in tabs
+function openTab(tabName) {
+
+    // //Selecting Divs && Current Active Div
+    let div = document.getElementsByClassName("div");
+    let activeDiv = document.getElementById(tabName);
+
+
+    // //Selecting Tabs && Current Active Tab
+    let tab = document.getElementsByClassName("tabs-button");
+    let activeTab = document.getElementById(tabName + "-tab");
+
+    // // Loop through to remove classes
+    for (var i = 0; i < tab.length; i++) {
+
+        div[i].classList.remove("show");
+        div[i].classList.add("hidden");
+
+        tab[i].classList.remove("tabs-open");
     }
 
-    function enter() {
-
-        let name = document.getElementById("name").value;
-
-        if (name) {
-
-            localStorage.setItem("name", name);
-            location.href = baseUrl + "/views/dashboard.html";
-        } else {
-
-            alert("Nickname is required!");
-        }
-    }
-
-    function home() {
-
-        localStorage.removeItem("name");
-        location.href = baseUrl + '/home.html'
-        console.log(baseUrl);
-    }
-
-    function openTab(tabName) {
-        var i;
-        var x = document.getElementsByClassName("tab");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        document.getElementById(tabName).style.display = "block";
-    }
+    activeDiv.classList.add("show");
+    activeTab.classList.add("tabs-open");
+}
--- public/js/quotes.js
+++ public/js/quotes.js
@@ -1,31 +1,38 @@
-function quoteToday() {
+let quotes = [{
+        author: "E.E.Cummings",
+        quote: "It takes courage to grow up and become who you really are."
+    },
+    {
+        author: "Dr. Seuss",
+        quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You're on your own. And you know what you know. And you are the guy who'll decide where to go."
+    },
+    {
+        author: "C.S. Lewis",
+        quote: "You are never too old to set another goal or to dream a new dream."
+    },
+    {
+        author: "Viola Davis",
+        quote: "All dreams are within reach. All you have to do is keep moving towards them."
+    },
+    {
+        author: "Oscar Wilde",
+        quote: "To live is the rarest thing in the world. Most people just exist."
+    }
+]
 
-    let quoteToday = [{
-            author: "E.E.Cummings",
-            quote: "It takes courage to grow up and become who you really are."
-        },
-        {
-            author: "Dr. Seuss",
-            quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. You're on your own. And you know what you know. And you are the guy who'll decide where to go."
-        },
-        {
-            author: "C.S. Lewis",
-            quote: "You are never too old to set another goal or to dream a new dream."
-        },
-        {
-            author: "Viola Davis",
-            quote: "All dreams are within reach. All you have to do is keep moving towards them."
-        },
-        {
-            author: "Oscar Wilde",
-            quote: "To live is the rarest thing in the world. Most people just exist."
-        }
-    ]
 
-    let random = Math.random() * quoteToday.length;
+// //Ranmize Quotes
+function quoteToday(quotes) {
 
-    document.getElementById('author').innerText = "~"+quoteToday[Math.floor(random)].author;
-    document.getElementById('quote').innerText = quoteToday[Math.floor(random)].quote;
+    let random = Math.floor((Math.random() * quotes.length));
+
+    document.getElementById('author').innerText = "~" + quotes[random].author;
+    document.getElementById('quote').innerText = quotes[random].quote;
 }
 
-quoteToday()
+quoteToday(quotes);
+
+// //Refresh Quote
+function refreshQuote() {
+    quoteToday(quotes);
+}
